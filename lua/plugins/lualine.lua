0526c7a605ee5e512e26194af85bf3e308544302
anurag <anuragpatil888825@gmail.com>
2024-04-28 01:05:23 +0530

    first commit

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,14 @@
+return {
+    
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+
+    local lualine = require("lualine").setup()
+    options = { theme = 'auto'}
+    end
+
+
+}
+    
+
